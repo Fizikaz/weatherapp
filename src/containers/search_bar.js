204dae6437bb6ac84d029c753e04969389c83da6
Mindaugas <mindaugojuskos@gmail.com>
2018-05-10 16:15:34 +0300

    application complete

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -31,12 +31,12 @@ class SearchBar extends Component {
         return (
             <form onSubmit={this.onFormSubmit} className="input-group">
               <input 
-                placeholder="Get a five-day forecast in your favorite cities"
+                placeholder="Gaukite ateinančių 5 dienų prognozę savo įvestuose miestuose"
                 className="form-control"
                 value={this.state.term}
                 onChange={this.onInputChange} />
               <span className="input-group-btn">
-                <button type="submit" className="btn btn-secondary">Submit</button>
+                <button type="submit" className="btn btn-secondary">Pateikti</button>
               </span>
             </form>
         );
